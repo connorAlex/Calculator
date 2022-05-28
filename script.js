e2f305824ad0fa392527aa13a288885e6aff073a
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-28 16:35:21 -0400

    add equal functionality

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 //add event listener to "collect" digits for the first number
 //once operator is pressed, the first digit is passed to another var and the next digit is calculated
 
+let btns = document.querySelectorAll(".num");
+let operatorList = document.querySelectorAll(".op");
+let cached = document.querySelector(".history");
+let digit = document.querySelector(".numDisplay");
+let equals = document.querySelector(".equals");
 
+btns.forEach(btns => btns.addEventListener('click', numEvent));
+operatorList.forEach( operatorList => operatorList.addEventListener('click', function() {
+    cache(digit.innerHTML,operatorList.value);
+}));
 
-
+equals.addEventListener("click", function() {
+    let funct = cached.innerHTML + " " + digit.innerHTML;
+    cached.innerHTML = funct;
+    digit.innerHTML = parseCalc(funct);
+});
 
 function parseCalc(calc){
     return new Function ('return ' + calc)();
 }
-console.log(parseCalc("2+2"));
+
+function numEvent(e) {
+    digit.innerHTML = e.target.value;
+    console.log(digit);
+}
+
+function cache(num,op){
+    //build the string to be executed by the parseCalc function
+    let cachFunc = num + " " + op;
+    cached.innerHTML = cachFunc;
+}
