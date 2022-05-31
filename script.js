647e5d0e3fa2b28ed156d94f5c97b8ef311d8c59
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-31 07:31:02 -0500

    add clear/delete html buttons

--- script.js
+++ script.js
@@ -6,29 +6,32 @@ let operatorList = document.querySelectorAll(".op");
 let cached = document.querySelector(".history");
 let digit = document.querySelector(".numDisplay");
 let equals = document.querySelector(".equals");
+let calcComplete = false;
 
 btns.forEach(btns => btns.addEventListener('click', numEvent));
 operatorList.forEach( operatorList => operatorList.addEventListener('click', function() {
     cache(digit.innerHTML,operatorList.value);
 }));
 
-equals.addEventListener("click", function() {
+equals.addEventListener("click", operate);
+
+function operate(){
     let funct = cached.innerHTML + " " + digit.innerHTML;
-    cached.innerHTML = funct;
+    cached.innerHTML = funct + " =";
     digit.innerHTML = parseCalc(funct);
-});
+}
 
 function parseCalc(calc){
     return new Function ('return ' + calc)();
 }
 
 function numEvent(e) {
-    digit.innerHTML = e.target.value;
-    console.log(digit);
+    digit.innerHTML += e.target.value;
 }
 
 function cache(num,op){
     //build the string to be executed by the parseCalc function
     let cachFunc = num + " " + op;
     cached.innerHTML = cachFunc;
+    digit.innerHTML = "";
 }
